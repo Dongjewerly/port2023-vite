35cfabfd7e119f17a54733ea25809fb6f0c68d0e
Dongseok <117716335+Dongjewerly@users.noreply.github.com>
2024-04-11 14:00:18 +0900

    헤더완료

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,23 @@
 export function menu() {
-    console.log(menu)
+    const headerToggle = document.getElementById("headerToggle");
+    const headerNav = document.querySelector(".header_nav");
+
+    if (headerToggle) {
+        headerToggle.addEventListener("click", () => {
+            headerNav.classList.toggle("show");
+    
+            headerToggle.getAttribute("aria-expanded") == "true" 
+            ? headerToggle.setAttribute("aria-expanded", "false") 
+            : headerToggle.setAttribute("aria-expanded" , "true");
+    
+            // console.log(headerToggle.getAttribute("aria-expanded")); 
+            // get은 얻기 set은 설정
+            // if (headerToggle.getAttribute("aria-expanded") == "true" ) {
+            //     headerToggle.setAttribute("aria-expanded", "false") 
+            // } else {
+            //     headerToggle.setAttribute("aria-expanded" , "true");
+            // }
+        
+        });
+    }
 }
